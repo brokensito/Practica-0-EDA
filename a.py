2304b5c12ee5758c8ef099e2bcef1d022c322acb
David Sanz <dsanzfuertes.15@gmail.com>
2024-10-06 18:25:50 +0200

    EJ5

--- a.py
+++ a.py
@@ -1,3 +1,14 @@
-from datetime import date
+def frase_vocales():
+    usuario = input("Escribe una frase: ")
+    vocales = ["a","e","i","o","u"]
+    contador = 0
 
-print(f"{date.today().year}")
+    for i in usuario:
+        if i in vocales:
+            contador+=1
+
+    return contador
+
+
+if __name__=="__main__":
+    print(frase_vocales())
