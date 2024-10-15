11edd9528878a3cc692ccee7602d7a712160fe15
David Sanz <113629537+brokensito@users.noreply.github.com>
2024-10-15 10:54:48 +0200

    ej1

--- ej1.py
+++ ej1.py
@@ -4,22 +4,13 @@ PRÁCTICA 0 Estructura de Datos y Algoritmos IM. Curso 24/25
 - EJERCICIO: 1  
 - EXPLICACIONES: 
 
-- En la funcion nacimiento() se implementa el modulo datetime para sacar el año actual
+- En la funcion nacimiento() se implementa el modulo datetime para sacar el año actual, de manera que se resta con el año introducido por el 
+usuario para sacar su edad actual.
+- Se han hecho excepciones para evaluar la mayoria de los casos en las diferentes funciones.
+- Al principio del programa se establece un bucle para introducir la contraseña y que se conceda el acceso unicamente si es la correcta,
+si es diferente se le pide al usuario de nuevo. 
+- En los demas apartados en vez de utilizar bucles se utiliza recursividad.
 
-
-
-20. Pide al usuario que ingrese una contraseña. Si la contraseña es 'python123',
-imprime 'Acceso concedido'. Si no, imprime 'Acceso denegado'.
-
-16.Pide al usuario que ingrese su edad. Si es mayor de 18, imprime 'Eres mayor
-de edad'. Si no, imprime 'Eres menor de edad'
-
-19. Pide al usuario que ingrese su altura en metros. Si la altura es mayor a 1.70,
-imprime 'Eres alto'. Si es menor o igual a 1.70, imprime 'Eres de estatura
-promedio'.
-
-25. Pide al usuario que ingrese su año de nacimiento. Calcula su edad en base al
-año actual y muestra si la persona es mayor o menor de 30 años --> usar datetime
 
 '''
 
@@ -82,8 +73,6 @@ def nacimiento():
     return resultado
     
 
-    return resultado
-
 if __name__=="__main__":
     var = False
 
@@ -123,7 +112,7 @@ if __name__=="__main__":
         print("Eres menor de 30 años.\n")
 
 
-    print("********* FIN DEL PROGRAMA *********")
+    print("********* FIN DEL PROGRAMA ***********")
 
 
 
